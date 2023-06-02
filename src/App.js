b95ce47df5966e8d2d407b80e00455907b7805ad
Farhan <fsmasood@mun.ca>
2023-06-02 08:32:06 -0230

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return <h1><><Header />
+    <Meme /></></h1>
+}
+
